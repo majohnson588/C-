ef5c736bba2bfc543d4aa397d9f41b19b6bf287c
majohnson588 <117084422+majohnson588@users.noreply.github.com>
2023-04-26 11:15:45 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -123,3 +123,260 @@ int main()
 	
 	return 0;
 }
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Time
+{
+public:
+	//Time(int hour = 0)
+	Time(int hour)
+	{
+		_hour = hour;
+	}
+private:
+	int _hour;
+};
+
+class Date
+{
+public:
+	Date(int year, int hour, int& x)
+		: _year(year)
+		, _t(hour)
+	    , _N(10)
+		, _ref(x)
+	{ 
+		// 函数体内初始化 
+		_year = year;
+		_ref++;
+	}
+private:
+	// 声明
+	int _year = 0;  // C++11 缺省值 -- 初始化列表时没有显示给值就会用这个缺省值
+	Time _t;
+	const int _N;
+	int& _ref;
+};
+
+// 结论：自定义类型成员，推荐用初始化列表初始化
+// 初始化列表可以认为是成员变量定义的地方
+
+//内置类型也推荐使用初始化列表，当然内置类型在函数体内初始化也没有什么明显的问题
+//能使用初始化列表就使用，使用初始化列表基本没什么毛病
+
+//int main()
+//{
+//	int y = 0;
+//	Date d(2022, 1, y);
+//	const int N; // const必须在定义的地方初始化，只有一次机会
+//
+//	return 0;
+//}
+
+////////////////////////////////////////////////////////////////////////////
+//class A
+//{
+//public:
+//	// 有些初始化工作还是必须在函数体内完成
+//    A(int N)
+//		:_N(N)
+//	{
+//		 _a = ((int*)malloc(sizeof(int)*N))
+//		, _N(N)
+//		if (_a == NULL)
+//		{
+//			perror("malloc fail\n");
+//		}
+//		memset(_a, 0, sizeof(int) * N);
+//	}
+//private:
+//	// 声明
+//	int* _a;
+//	int _N;
+//};
+
+class A
+{
+public:
+	// 初始化按声明顺序初始化
+	A(int a)
+	// 成员变量定义
+		:_a1(a)
+		, _a2(_a1)
+	{}
+
+	void Print() {
+		cout << _a1 << " " << _a2 << endl;
+	}
+private:
+	// 成员变量声明
+	int _a2;
+	int _a1;
+};
+
+//A.输出1  1
+//B.程序崩溃
+//C、编译不通过
+//D、输出1  随机值
+
+//int main()
+//{
+//	// D
+//	// 对象定义
+//	A aa1(1);
+//	aa1.Print();
+//
+//	A aa2(2);
+//
+//	return 0;
+//} 
+
+/////////////////////////////////////////////////////////////
+//explicit关键字 + 匿名对象
+class Date
+{
+public:
+	explicit Date(int year)
+		:_year(year)
+	{
+		cout << "Date(int year)" << endl;
+	}
+
+	Date(const Date& d)
+	{
+		cout << "Date (const Date& d)" << endl;
+	}
+
+	~Date()
+	{
+		cout << "Date(const Date& d)" << endl;
+	}
+private:
+	int _year;
+};
+
+void func(const string& s)
+{}
+
+class Solution {
+public:
+	int Sum_Solution(int n) {
+		// ...
+		return 0;
+	}
+};
+
+//int main()
+//{
+//	Date d1(2022);  // 直接调用构造
+//	Date d2 = 2022; // 隐式类型转换：构造 + 拷贝构造 + 优化 -> 直接调用构造
+//	const Date& d3 = 2022;
+//
+//	int i = 10;
+//	const double& d = i;
+//
+//	string s1("hello");
+//	string s2 = "hello";
+//
+//	string str("insert");
+//	func(str);
+//	func("insert");
+//
+//	Date d4(2000);
+//
+//	//匿名对象，一些使用场景
+//	Solution slt;
+//	slt.Sum_Solution(10);
+//	Solution().Sum_Solution(10);
+//
+//	// 匿名对象 - 生命周期只有这一行
+//	Date(2000);
+//
+//	return 0;
+//}
+
+//设计一个只能在栈上定义对象的类
+class StackOnly
+{
+public:
+	static StackOnly CreateObj()
+	{
+		StackOnly so;
+	    return so;
+	}
+private:
+	StackOnly(int x = 0,int y = 0)
+		:_x(x)
+		, _y(0)
+	{}
+private:
+	int _x = 0;
+	int _y = 0;
+};
+
+//int main()
+//{
+//	//StackOnly so1; // 栈
+//	//static StackOnly so2;  // 静态区
+//	StackOnly so3 = StackOnly::CreateObj();
+//
+//	return 0;
+//}
+
+class A
+{
+private:
+	int _h;
+public:
+    // B定义在A的里面
+	// 1、受A的类域限制，访问限定符
+	// 2、B天生是A的友元
+	class B 
+	{
+	public:
+		void foo(const B& a)
+		{
+			cout << k << endl; // OK
+			cout << a._b << endl; // OK -- 友元
+		}
+	private:
+		int _b;
+	};
+};
+int A::k = 1;
+
+class ListNode
+{
+private:
+	ListNode* _next;
+	int _val;
+};
+
+class List
+{
+	class ListNode
+	{
+	private:
+		ListNode* _next;
+		int _val;
+	};
+public:
+	void PushBack()
+	{
+		ListNode node;
+		node->_val;
+	}
+private:
+	ListNode* _head;
+};
+int main()
+{
+	cout << sizeof(A) << endl; // 4
+	A a;
+	A::B b;
+
+	return 0;
+}
